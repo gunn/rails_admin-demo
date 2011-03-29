a2da7eed479d4f7ed6830735d725f2d064e90300
Arthur Gunn <arthur@gunn.co.nz>
2011-03-29 19:58:34 +1300

    Re-migrated, generated possibly more postgres friendly data

--- db/schema.rb
+++ db/schema.rb
@@ -127,17 +127,8 @@ ActiveRecord::Schema.define(:version => 20110224184303) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
-    t.string   "reset_password_token"
-    t.string   "remember_token"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "email"
+    t.integer  "sign_in_count"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar_file_name"
@@ -146,8 +137,5 @@ ActiveRecord::Schema.define(:version => 20110224184303) do
     t.datetime "avatar_updated_at"
     t.string   "roles"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
